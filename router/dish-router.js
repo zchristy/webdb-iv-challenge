6f8bfa37ef175e7f0335a23c74bb5b17a912a478
zchristy <zchristy44@gmail.com>
2019-06-07 02:20:33 -0500

    Complete Middleware for Routes

--- router/dish-router.js
+++ router/dish-router.js
@@ -2,17 +2,17 @@ const express = require('express');
 
 const db = require('../data/data-model.js')
 
-// const mw = require('../middleware/middleware.js')
+const mw = require('../middleware/middleware.js')
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  db.addDish(req.body)
+router.post('/', mw.validateDish, (req, res) => {
+  db.addDish(req.dish)
   .then(id => {
     res.status(200).json(id)
   })
   .catch(err => {
-    res.status(500).json(err)
+    res.status(500).json(err.message)
   })
 });
 
@@ -22,16 +22,16 @@ router.get('/', (req, res) => {
     res.status(200).json(dishes)
   })
   .catch(err => {
-    res.status(500).json(err)
+    res.status(500).json(err.message)
   })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', mw.validateDishId, (req, res) => {
 
-  db.getDish(req.params.id)
+  db.getDish(req.dishId)
   .then(dish => {
 
-    db.getRecipesByDish(req.params.id)
+    db.getRecipesByDish(req.dishId)
     .then(recipes => {
 
       const dishObj = dish
@@ -50,23 +50,23 @@ router.get('/:id', (req, res) => {
   })
 });
 
-router.put('/:id', (req, res) => {
-  db.updateDish(req.params.id, req.body)
+router.put('/:id', mw.validateDishId, mw.validateDish, (req, res) => {
+  db.updateDish(req.dishId, req.dish)
   .then(dish => {
     res.status(200).json(dish)
   })
   .catch(err => {
-    res.status(500).json(err)
+    res.status(500).json(err.message)
   })
 });
 
-router.delete('/:id', (req, res) => {
-  db.removeDish(req.params.id)
+router.delete('/:id', mw.validateDishId, (req, res) => {
+  db.removeDish(req.dishId)
   .then(dish => {
     res.status(200).json({message: "Dish succesfully Deleted"})
   })
   .catch(err => {
-    res.status(500).json(err)
+    res.status(500).json(err.message)
   })
 });
 
--- router/recipe-router.js
+++ router/recipe-router.js
@@ -2,17 +2,17 @@ const express = require('express');
 
 const db = require('../data/data-model.js')
 
-// const mw = require('../middleware/middleware.js')
+const mw = require('../middleware/middleware.js')
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  db.addRecipe(req.body)
+router.post('/', mw.validateRecipe, (req, res) => {
+  db.addRecipe(req.recipe)
   .then(id => {
     res.status(200).json(id)
   })
   .catch(err => {
-    res.status(500).json(err)
+    res.status(500).json(err.message)
   })
 });
 
@@ -22,16 +22,16 @@ router.get('/', (req, res) => {
     res.status(200).json(recipes)
   })
   .catch(err => {
-    res.status(500).json(err)
+    res.status(500).json(err.message)
   })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', mw.validateRecipeId, (req, res) => {
 
-  db.getRecipe(req.params.id)
+  db.getRecipe(req.recipeId)
   .then(recipe => {
 
-    db.getIngredientsByRecipe(req.params.id)
+    db.getIngredientsByRecipe(req.recipeId)
     .then(ingredients => {
 
       const recipeObj = recipe
@@ -50,23 +50,23 @@ router.get('/:id', (req, res) => {
   })
 });
 
-router.put('/:id', (req, res) => {
-  db.updateRecipe(req.params.id, req.body)
+router.put('/:id', mw.validateRecipeId, mw.validateRecipe, (req, res) => {
+  db.updateRecipe(req.recipeId, req.recipe)
   .then(recipe => {
     res.status(200).json(recipe)
   })
   .catch(err => {
-    res.status(500).json(err)
+    res.status(500).json(err.message)
   })
 });
 
-router.delete('/:id', (req, res) => {
-  db.removeRecipe(req.params.id)
+router.delete('/:id', mw.validateRecipeId, (req, res) => {
+  db.removeRecipe(req.recipeId)
   .then(recipe => {
     res.status(200).json({message: "Recipe succesfully Deleted"})
   })
   .catch(err => {
-    res.status(500).json(err)
+    res.status(500).json(err.message)
   })
 });
 
